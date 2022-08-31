0b319ad00b9af9b183ff9c88da3d8ea1c7e61569
Gabriel <gabxyzdev@gmail.com>
2022-08-31 14:38:03 -0300

    Refactor to remove styled-components
    * This commit removes styled-components from dependencies along with its previously defined configs.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,13 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
-import GlobalStyles from 'styles/global'
-
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Gabxyz</title>
-        <meta
-          name="description"
-          content="Personal portfolio project built with TypeScript, React, NextJS and Styled Components"
-        />
+        <meta name="description" content="Personal portfolio project" />
       </Head>
-      <GlobalStyles />
       <Component {...pageProps} />
     </>
   )
