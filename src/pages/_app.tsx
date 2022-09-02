650898c3de6d6aea17c858dcf98b66a811f7fcef
Gabriel <gabxyzdev@gmail.com>
2022-09-02 14:37:32 -0300

    Add ThemeProvider from next-themes and globalStyles from stitches

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,22 @@
+import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { darkTheme, globalStyles } from 'stitches.config'
 
 function App({ Component, pageProps }: AppProps) {
+  globalStyles()
   return (
-    <>
+    <ThemeProvider
+      attribute="class"
+      value={{ light: 'light-theme', dark: darkTheme.className }}
+      disableTransitionOnChange
+    >
       <Head>
         <title>Gabxyz</title>
         <meta name="description" content="Personal portfolio project" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
