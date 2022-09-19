daf44fe48fce08da3f65ef27c8ba257f3b35238e
Gabriel <gabxyzdev@gmail.com>
2022-09-19 14:44:46 -0300

    Add reusable Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import cn from 'classnames'
+
+interface ButtonProps<T extends React.ElementType> {
+  as?: T
+  size?: 'sm' | 'md' | 'lg'
+  variant?: 'default' | 'active' | 'social'
+  children?: React.ReactNode
+}
+
+const Button = <T extends React.ElementType = 'button'>({
+  size = 'md',
+  variant = 'default',
+  as,
+  children,
+  ...rest
+}: ButtonProps<T> &
+  Omit<React.ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>) => {
+  const Wrapper = as || 'button'
+  return (
+    <Wrapper {...rest} className={cn(`btn ${size} ${variant}`)}>
+      {children}
+    </Wrapper>
+  )
+}
+
+export default Button
