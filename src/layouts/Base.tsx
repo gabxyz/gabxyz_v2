f71ef69276b5aee8fd92d63e18e4b91c180e55de
Gabriel <gabxyzdev@gmail.com>
2022-09-23 17:12:35 -0300

    Refactor layout component
    * Add Base.tsx component for persistent layout with motion animations in the main section
    
    * Remove previously used Layout folder

---
+++ src/layouts/Base.tsx
@@ -0,0 +1,49 @@
+import Nav from 'components/Nav'
+import { AnimatePresence, motion } from 'framer-motion'
+import { useRouter } from 'next/router'
+
+type BaseProps = {
+  children: React.ReactNode
+}
+
+const container = {
+  hidden: {
+    opacity: 0,
+    y: 20
+  },
+  show: {
+    opacity: 1,
+    y: 0
+  },
+  exit: {
+    opacity: 0
+  }
+}
+
+const Base = ({ children }: BaseProps) => {
+  const { asPath } = useRouter()
+  return (
+    <>
+      <Nav />
+      <AnimatePresence mode="wait">
+        <motion.main
+          key={asPath}
+          variants={container}
+          initial="hidden"
+          animate="show"
+          exit="exit"
+          transition={{
+            type: 'spring',
+            damping: 25,
+            stiffness: 250
+          }}
+          className="my-14 md:my-28"
+        >
+          {children}
+        </motion.main>
+      </AnimatePresence>
+    </>
+  )
+}
+
+export default Base
