6bccd3699aeca0d86c3e93e466bb5562257c18e9
Gabriel <gabxyzdev@gmail.com>
2022-10-03 10:35:10 -0300

    Redesign Card component
    This commit adds a simpler card structure with less data being shown in it. Also adds a Learn More button that will be used to redirect the user to a more detailed project page.

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,64 +1,53 @@
-import * as AspectRatio from '@radix-ui/react-aspect-ratio'
-import { CodeIcon, RocketIcon } from '@radix-ui/react-icons'
+import { ArrowRightIcon } from '@radix-ui/react-icons'
+import cn from 'classnames'
 import Button from 'components/Button'
-import Separator from 'components/Separator'
 import Image from 'next/image'
+import { useState } from 'react'
 
 interface CardProps {
   title: string
   description: string
-  tools: string
-  imgUrl: string
-  liveUrl?: string
-  repoUrl?: string
+  tools: string[]
+  bannerUrl: string
 }
 
-const Card = ({
-  title,
-  description,
-  tools,
-  imgUrl,
-  liveUrl,
-  repoUrl
-}: CardProps) => {
+const Card = ({ title, description, tools, bannerUrl }: CardProps) => {
+  const [isLoading, setLoading] = useState(true)
+
   return (
     <>
-      <div className="flex w-full flex-col items-center gap-4 rounded-xl bg-mauve-2 px-4 py-6 shadow-default hover:shadow-card-hover focus:shadow-card-focus md:w-2/3">
-        <h2 className="text-lg font-bold">{title}</h2>
-        <div className="w-full">
-          <AspectRatio.Root ratio={16 / 9} className="rounded-md shadow-image">
+      <div className="w-full max-w-md rounded-lg bg-mauve-2 shadow-lg">
+        <a className="group cursor-pointer">
+          <div className="aspect-w-16 aspect-h-9 relative w-full overflow-hidden rounded-t-md">
             <Image
-              className="rounded-md "
-              src={imgUrl}
+              src={bannerUrl}
               layout="fill"
               objectFit="cover"
+              className={cn(
+                'duration-500 ease-in-out group-hover:opacity-75',
+                isLoading ? 'grayscale blur-md' : 'grayscale-0 blur-0'
+              )}
+              onLoadingComplete={() => setLoading(false)}
             />
-          </AspectRatio.Root>
-        </div>
-        <div className="mt-4 flex flex-col items-start gap-4">
+          </div>
+        </a>
+
+        <div className="flex flex-col items-start gap-6 p-4">
           <section className="space-y-1">
-            <h2 className="font-semibold">About this project</h2>
-            <p className="text-mauve-11">{description}</p>
+            <h2 className="text-xl font-bold">{title}</h2>
+            <p className="font-mono text-sm font-semibold text-mauve-11">
+              {description}
+            </p>
           </section>
-          <Separator />
-          <section className="space-y-1">
-            <h2 className="font-semibold">Tools used</h2>
-            <p className="text-mauve-11">{tools}</p>
-          </section>
-          <Separator />
-          <section className="space-y-2">
-            <h2 className="font-semibold">Resources</h2>
-            <div className="space-x-2 text-sm font-semibold text-mauve-11 md:space-x-4">
-              <Button as="a" href={liveUrl} target="_blank">
-                <RocketIcon />
-                <p className="ml-1">Live project</p>
-              </Button>
-              <Button as="a" href={repoUrl} target="_blank">
-                <CodeIcon />
-                <p className="ml-1">Code repository</p>
-              </Button>
-            </div>
-          </section>
+          <div className="flex w-full items-center justify-between">
+            <p className="font-mono text-sm font-bold">{tools.join(' :: ')}</p>
+            <Button as="a">
+              <span className="mr-2 font-mono text-sm font-bold">
+                Learn more
+              </span>
+              <ArrowRightIcon />
+            </Button>
+          </div>
         </div>
       </div>
     </>
